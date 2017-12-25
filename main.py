504851cb8006d2c5a790c9b3066e12bca44d1eea
Chris Gresock <chris.gresock@gmail.com>
2017-12-25 13:46:12 -0500

    added feature

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from support import apa102
+from support import apa102, colorschemes
 from weather import Weather
+from colour import Color
 import time
 
 num_led = 10
 woeid = 2347592
+
+top_high = 100
+bottom_low = 10
+color_range = top_high - bottom_low
+
+blue = Color("blue")
+colors = list(blue.range_to(Color("red"), color_range))
 
 def get_high_low():
     weather = Weather()
@@ -14,4 +22,5 @@ def get_high_low():
 if __name__ == "__main__":
     weather = get_high_low()
     print(weather)
+    print(colors[weather[0]])
     pass
